a6fb4802912e5f5c8dba2af1b22d9cb7ac151a9b
Saravana Balaji A J <99321740+SaravanaBalaji02@users.noreply.github.com>
2024-07-13 11:38:57 +0530

    Time: 23 ms (11.98%), Space: 42.3 MB (43.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,30 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length())
-        {
-            return false;
-        }
-        HashSet<Character> set1=new HashSet<>();
-        HashSet<Character> set2=new HashSet<>();
-        for(int i=0;i<s.length();i++)
-        {
-            set1.add(s.charAt(i));
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            set2.add(t.charAt(i));
-        }
-        if(set1.size()==set2.size())
-        {
-            return true;
-        }
-        return false;
-
-
+  public boolean isIsomorphic(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
     }
+
+    HashMap<Character, Character> mapS = new HashMap<>();
+    HashMap<Character, Character> mapT = new HashMap<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char charS = s.charAt(i);
+      char charT = t.charAt(i);
+
+   
+      if (mapS.containsKey(charS) && mapS.get(charS) != charT) {
+        return false;
+      }
+
+ 
+      if (mapT.containsKey(charT) && mapT.get(charT) != charS) {
+        return false;
+      }
+
+      mapS.put(charS, charT);
+      mapT.put(charT, charS);
+    }
+
+    return true;
+  }
 }
