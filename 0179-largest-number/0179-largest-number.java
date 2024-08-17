d72564969219debdd10b542269e0c770399cdfc8
Saravana Balaji A J <99321740+SaravanaBalaji02@users.noreply.github.com>
2024-08-17 11:32:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -6,17 +6,62 @@ class Solution {
       {
         str[i]=Integer.toString(nums[i]);
       }
-      Arrays.sort(str,(s1,s2)->(s1+s2).compareTo(s2+s1));
+     for(int i=0;i<str.length;i++)
+     {
+        for(int j=i+1;j<str.length-i-1;j++)
+        {
+            String s1=str[j]+str[j+1];
+            String s2=str[j+1]+str[j];
+            if(compare(s1,s2)<0)
+            {
+                String temp=str[j];
+                str[j]=str[j+1];
+                str[j+1]=temp;
+            }
+        }
+     }
       StringBuilder sb=new StringBuilder("");
       for(int i=nums.length-1;i>=0;i--)
       {
         sb.append(str[i]);
       }
-      if(sb.charAt(i)=='0')
+      if(sb.charAt(0)=='0')
       {
        return "0";
       }
       return new String(sb);
 
+}
+
+
+public static int compare(String s1,String s2)
+{
+    int len=Math.min(s1.length(),s2.length());
+    for(int i=0;i<len;i++)
+    {
+        char c1=s1.charAt(i);
+        char c2=s2.charAt(i);
+        if(c1>c2)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    if(s1.length()>s2.length())
+    {
+        return 1;
+    }
+    else if(s1.length()<s2.length())
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 }
