f01dcf285951fc9a54307b5bb20fa5965faceca2
Saravana Balaji A J <99321740+SaravanaBalaji02@users.noreply.github.com>
2024-08-17 11:56:16 +0530

    Time: 11 ms (10.92%), Space: 45.2 MB (5.32%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,67 +1,34 @@
 class Solution {
     public String largestNumber(int[] nums) {
-   
-      String str[]=new String[nums.length];
-      for(int i=0;i<str.length;i++)
-      {
-        str[i]=Integer.toString(nums[i]);
-      }
-     for(int i=0;i<str.length;i++)
-     {
-        for(int j=i+1;j<str.length-i-1;j++)
-        {
-            String s1=str[j]+str[j+1];
-            String s2=str[j+1]+str[j];
-            if(compare(s1,s2)<0)
-            {
-                String temp=str[j];
-                str[j]=str[j+1];
-                str[j+1]=temp;
+        String str[] = new String[nums.length];
+        for (int i=0; i<nums.length;i++) {
+            str[i] = Integer.toString(nums[i]);
+        }
+        
+        for (int i=0; i<str.length-1;i++) {
+            for (int  j=0; j<str.length-i-1;j++) {
+                String s1=str[j] +str[j + 1];
+                String s2=str[j + 1]+str[j];
+                
+                if (s1.compareTo(s2)<0) {
+                    String temp =str[j];
+                    str[j]=str[j + 1];
+                    str[j+1]=temp;
+                }
             }
         }
-     }
-      StringBuilder sb=new StringBuilder("");
-      for(int i=nums.length-1;i>=0;i--)
-      {
-        sb.append(str[i]);
-      }
-      if(sb.charAt(0)=='0')
-      {
-       return "0";
-      }
-      return new String(sb);
-
-}
-
-
-public static int compare(String s1,String s2)
-{
-    int len=Math.min(s1.length(),s2.length());
-    for(int i=0;i<len;i++)
-    {
-        char c1=s1.charAt(i);
-        char c2=s2.charAt(i);
-        if(c1>c2)
-        {
-            return 1;
+        
+        StringBuilder sb =new StringBuilder();
+        for (int i=0; i<str.length; i++) {
+            sb.append(str[i]);
         }
-        else
-        {
-            return -1;
+        
+        if (sb.charAt(0) == '0') {
+            return "0";
         }
+        
+        return sb.toString();
     }
 
-    if(s1.length()>s2.length())
-    {
-        return 1;
-    }
-    else if(s1.length()<s2.length())
-    {
-        return -1;
-    }
-    else
-    {
-        return 0;
-    }
-}
+   
 }
